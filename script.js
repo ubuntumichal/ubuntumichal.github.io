226b61b5796fa3a11339f17758bbe2e5777dc667
ubuntumichal <84479944+ubuntumichal@users.noreply.github.com>
2023-02-28 16:30:05 -1000

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 // define constants
 const CARD_VALUES = {
-'ACE': 11,
-'2': 2,
-'3': 3,
-'4': 4,
-'5': 5,
-'6': 6,
-'7': 7,
-'8': 8,
-'9': 9,
-'10': 10,
-'JACK': 10,
-'QUEEN': 10,
-'KING': 10,
+  'ACE': 11,
+  '2': 2,
+  '3': 3,
+  '4': 4,
+  '5': 5,
+  '6': 6,
+  '7': 7,
+  '8': 8,
+  '9': 9,
+  '10': 10,
+  'JACK': 10,
+  'QUEEN': 10,
+  'KING': 10,
 };
 const CARDS = ['ACE', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'JACK', 'QUEEN', 'KING'];
 const STARTING_MONEY = 100;
@@ -40,164 +40,162 @@ updateBet();
 
 // define functions
 function shuffleDeck() {
-deck = [];
-for (let i = 0; i < 4; i++) {
-deck = deck.concat(CARDS);
-}
-deck = shuffleArray(deck);
+  deck = [];
+  for (let i = 0; i < 4; i++) {
+    deck = deck.concat(CARDS);
+  }
+  deck = shuffleArray(deck);
 }
 
 function shuffleArray(array) {
-let currentIndex = array.length;
-let temporaryValue;
-let randomIndex;
+  let currentIndex = array.length;
+  let temporaryValue;
+  let randomIndex;
 
-while (0 !== currentIndex) {
-randomIndex = Math.floor(Math.random() * currentIndex);
-currentIndex -= 1;
-temporaryValue = array[currentIndex];
-array[currentIndex] = array[randomIndex];
-array[randomIndex] = temporaryValue;
-}
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
 
-return array;
+  return array;
 }
 
 function updateMoney() {
-document.getElementById('money').innerHTML = Money: ${money};
+  document.getElementById('money').innerHTML = `Money: ${money}`;
 }
 
 function updateBet() {
-document.getElementById('bet').innerHTML = Bet: ${bet};
+  document.getElementById('bet').innerHTML = `Bet: ${bet}`;
 }
 
 function deal() {
-// check if bet is within limits and player has enough money
-if (bet < MIN_BET || bet > MAX_BET || bet > money) {
-alert(Please enter a bet between ${MIN_BET} and ${MAX_BET} and within your budget of ${money}.);
-return;
-}
+  // check if bet is within limits and player has enough money
+  if (bet < MIN_BET || bet > MAX_BET || bet > money) {
+    alert(`Please enter a bet between ${MIN_BET} and ${MAX_BET} and within your budget of ${money}.`);
+    return;
+  }
 
-// reset scores and hands
-dealerScore = 0;
-playerScore = 0;
-dealerHand = [];
-playerHand = [];
+  // reset scores and hands
+  dealerScore = 0;
+  playerScore = 0;
+  dealerHand = [];
+  playerHand = [];
 
-// deal initial cards
-dealerHand.push(deck.pop());
-dealerScore += CARD_VALUES[dealerHand[0]];
-updateDealerCards();
+  // deal initial cards
+  dealerHand.push(deck.pop());
+  dealerScore += CARD_VALUES[dealerHand[0]];
+  updateDealerCards();
 
-playerHand.push(deck.pop());
-playerScore += CARD_VALUES[playerHand[0]];
-updatePlayerCards();
+  playerHand.push(deck.pop());
+  playerScore += CARD_VALUES[playerHand[0]];
+  updatePlayerCards();
 
-dealerHand.push(deck.pop());
-dealerScore += CARD_VALUES[dealerHand[1]];
-updateDealerCards();
+  dealerHand.push(deck.pop());
+  dealerScore += CARD_VALUES[dealerHand[1]];
+  updateDealerCards();
 
-playerHand.push(deck.pop());
-playerScore += CARD_VALUES[playerHand[1]];
-updatePlayerCards();
+  playerHand.push(deck.pop());
+  playerScore += CARD_VALUES[playerHand[1]];
+  updatePlayerCards();
 
-// check if player has blackjack
-if (playerScore === 21) {
-endRound(true, 'Blackjack!');
-return;
-}
+  // check if player has blackjack
+  if (playerScore === 21) {
+    endRound(true, 'Blackjack!');
+    return;
+  }
 
-// check if dealer has blackjack
-if (dealerScore === 21) {
-endRound(false, 'Dealer has blackjack.');
-return;
-}
+  // check if dealer has blackjack
+  if (dealerScore === 21) {
+    endRound(false, 'Dealer has blackjack.');
+    return;
+  }
 
-// enable/disable buttons
-document.getElementById('deal-button').disabled = true;
-document.getElementById('hit-button').disabled = false;
-document.getElementById('stand-button').disabled = false;
+  // enable/disable buttons
+  document.getElementById('deal-button').disabled = true;
+  document.getElementById('hit-button').disabled = false;
+  document.getElementById('stand-button').disabled = false;
 }
 
 function hit() {
-// add card to player's hand
+// deal a card to the player and update the score
 playerHand.push(deck.pop());
 playerScore += CARD_VALUES[playerHand[playerHand.length - 1]];
 updatePlayerCards();
 
 // check if player busts
 if (playerScore > 21) {
-endRound(false, 'You bust!');
-}
-
-// check if player gets blackjack
-if (playerScore === 21) {
-stand();
+endRound(false, 'Player busts.');
 }
 }
 
 function stand() {
-// disable buttons
+// disable hit and stand buttons
 document.getElementById('hit-button').disabled = true;
 document.getElementById('stand-button').disabled = true;
 
-// reveal dealer's second card
+// reveal the dealer's facedown card
 updateDealerCards(true);
 
-// dealer draws cards until score is at least 17
+// dealer hits until their score is 17 or higher
 while (dealerScore < 17) {
 dealerHand.push(deck.pop());
 dealerScore += CARD_VALUES[dealerHand[dealerHand.length - 1]];
-updateDealerCards();
+updateDealerCards(true);
 }
 
 // check if dealer busts
 if (dealerScore > 21) {
-endRound(true, 'Dealer busts!');
+endRound(true, 'Dealer busts.');
 return;
 }
 
-// compare scores
-if (dealerScore > playerScore) {
-endRound(false, 'Dealer wins!');
-} else if (dealerScore < playerScore) {
-endRound(true, 'You win!');
+// determine winner
+if (playerScore > dealerScore) {
+endRound(true, 'Player wins!');
+} else if (playerScore < dealerScore) {
+endRound(false, 'Dealer wins.');
 } else {
-endRound(true, 'Push!');
+endRound(false, 'Push.');
 }
 }
 
-function endRound(playerWins, message) {
-if (playerWins) {
+function updateDealerCards(revealSecondCard = false) {
+let html = '';
+for (let i = 0; i < dealerHand.length; i++) {
+html += <div class="card">${revealSecondCard || i !== 1 ? dealerHand[i] : 'X'}</div>;
+}
+document.getElementById('dealer-cards').innerHTML = html;
+document.getElementById('dealer-score').innerHTML = Score: ${revealSecondCard ? dealerScore : CARD_VALUES[dealerHand[0]]};
+}
+
+function updatePlayerCards() {
+let html = '';
+for (let i = 0; i < playerHand.length; i++) {
+html += <div class="card">${playerHand[i]}</div>;
+}
+document.getElementById('player-cards').innerHTML = html;
+document.getElementById('player-score').innerHTML = Score: ${playerScore};
+}
+
+function endRound(win, message) {
+if (win) {
 money += bet;
 } else {
 money -= bet;
 }
-alert(message);
 updateMoney();
+alert(message);
 
-// enable deal button and disable hit and stand buttons
-document.getElementById('deal-button').disabled = false;
-document.getElementById('hit-button').disabled = true;
-document.getElementById('stand-button').disabled = true;
-}
-
-function updateDealerCards(revealSecondCard) {
-let dealerCards = '';
-for (let i = 0; i < dealerHand.length; i++) {
-if (i === 1 && !revealSecondCard) {
-dealerCards += '<div class="card back"></div>';
+if (money < MIN_BET) {
+alert('Game over. You are out of money.');
+location.reload();
 } else {
-dealerCards += <div class="card ${dealerHand[i].toLowerCase()}"></div>;
+// reset bet and enable deal button
+bet = 0;
+updateBet();
+document.getElementById('deal-button').disabled = false;
 }
-}
-document.getElementById('dealer-cards').innerHTML = dealerCards;
-}
-
-function updatePlayerCards() {
-let playerCards = '';
-for (let i = 0; i < playerHand.length; i++) {
-playerCards += <div class="card ${playerHand[i].toLowerCase()}"></div>;
-}
-document.getElementById('player-cards').innerHTML = playerCards;
 }
