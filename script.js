5490adb3be5edf80659047a7699fbd9016060e3a
ubuntumichal <84479944+ubuntumichal@users.noreply.github.com>
2023-09-04 04:11:21 -1000

    Update script.js

--- script.js
+++ script.js
@@ -19,6 +19,8 @@ const jokerCards = [
     "joker-card-17.jpg",
 ];
 
+let drawnCards = []; // Array to store drawn cards
+
 // Function to reveal the card game section
 function revealCardGame() {
     const cardGame = document.getElementById("cardGame");
@@ -39,12 +41,26 @@ function drawCard() {
     // Disable the draw button temporarily
     drawButton.disabled = true;
 
-    // Randomly select a Joker card from the array
-    const randomIndex = Math.floor(Math.random() * jokerCards.length);
-    const selectedCard = jokerCards[randomIndex];
+    // Check if all cards have been drawn
+    if (drawnCards.length === jokerCards.length) {
+        cardImage.src = "all-cards-drawn.jpg"; // Replace with an image indicating all cards drawn
+        drawButton.innerText = "All Cards Drawn";
+    } else {
+        let randomIndex;
+        let selectedCard;
 
-    // Set the card image source
-    cardImage.src = selectedCard;
+        // Keep drawing until a unique card is drawn
+        do {
+            randomIndex = Math.floor(Math.random() * jokerCards.length);
+            selectedCard = jokerCards[randomIndex];
+        } while (drawnCards.includes(selectedCard));
+
+        // Add the drawn card to the drawnCards array
+        drawnCards.push(selectedCard);
+
+        // Set the card image source
+        cardImage.src = selectedCard;
+    }
 
     // Re-enable the draw button after a short delay
     setTimeout(() => {
