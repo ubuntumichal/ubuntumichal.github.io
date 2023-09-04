31d25dd20553e3abe3f33309cfac09a6618d91a1
ubuntumichal <84479944+ubuntumichal@users.noreply.github.com>
2023-09-04 00:41:11 -1000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+// Get the elements by their IDs
+const startButton = document.getElementById("startButton");
+const cardGame = document.getElementById("cardGame");
+const drawButton = document.getElementById("drawButton");
 
+// Add an event listener to the start button
+startButton.addEventListener("click", () => {
+    // Show the card game section
+    cardGame.style.display = "flex";
+});
+
+// Add an event listener to the draw button
+drawButton.addEventListener("click", () => {
+    // Simulate drawing a card (replace this with your game logic)
+    alert("You drew a Joker card!");
+});
