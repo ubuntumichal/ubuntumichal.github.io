0ebf083493391ebdaadb5c7a8dac3342ca232846
ubuntumichal <84479944+ubuntumichal@users.noreply.github.com>
2023-09-04 00:49:53 -1000

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-// Get the elements by their IDs
-const startButton = document.getElementById("startButton");
-const cardGame = document.getElementById("cardGame");
-const drawButton = document.getElementById("drawButton");
+// Array of Joker card image filenames
+const jokerCards = [
+    "joker-card-1.jpg",
+    "joker-card-2.jpg",
+    // Add all your Joker card image filenames here
+    // Ensure you have 52 images in the array
+];
 
-// Add an event listener to the start button
-startButton.addEventListener("click", () => {
-    // Show the card game section
-    cardGame.style.display = "flex";
-});
+// Function to randomly select a Joker card image
+function drawJokerCard() {
+    const randomIndex = Math.floor(Math.random() * jokerCards.length);
+    const selectedCard = jokerCards[randomIndex];
+    // Update the card image source
+    document.getElementById("cardImage").src = selectedCard;
+}
 
 // Add an event listener to the draw button
-drawButton.addEventListener("click", () => {
-    // Simulate drawing a card (replace this with your game logic)
-    alert("You drew a Joker card!");
+document.getElementById("drawButton").addEventListener("click", drawJokerCard);
+
+// Function to reveal the card game section
+document.getElementById("startButton").addEventListener("click", () => {
+    document.getElementById("cardGame").style.display = "flex";
 });
